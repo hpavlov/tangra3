aae4bb20e55f6633c54ca0444ac8fb461303003d
hristo <hristo_dpavlov@yahoo.com>
2012-11-20 10:34:02 +0000

    git-svn-id: http://canopus:81/svn/Tangra3/trunk@4 7d10f0b8-8c12-1b4f-80cc-3612f3cb94e2

--- Tangra.Core/Tangra.Core_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ TangraCore - Debug ]----------
-	@"$(MAKE)" -f "TangraCore.mk"
-clean:
-	@echo ----------Cleaning project:[ TangraCore - Debug ]----------
-	@"$(MAKE)" -f "TangraCore.mk" clean
