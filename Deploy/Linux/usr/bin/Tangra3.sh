e9d43fc0c6794dc2c2d4a4c40a771166e4373110
hristo <hristo_dpavlov@yahoo.com>
2013-08-04 09:51:56 +0000

    Removed files that shouldn't be part of the source control
    git-svn-id: http://canopus:81/svn/Tangra3/trunk@34 7d10f0b8-8c12-1b4f-80cc-3612f3cb94e2

--- Deploy/Linux/usr/bin/Tangra3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export DYLD_LIBRARY_PATH=/usr/local/lib/Tangra3:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/lib/Tangra3:$LD_LIBRARY_PATH
-exec /usr/bin/mono /usr/local/lib/Tangra3/Tangra.exe "$@"
--- Latest Binaries/Mac/Tangra.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export DYLD_LIBRARY_PATH=".";$DYLD_LIBRARY_PATH
-mono Tangra.exe
