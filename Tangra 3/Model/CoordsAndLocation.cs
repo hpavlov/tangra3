d1e745c6769dddbf0e671e15372627e5cc2d7e0a
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2015-06-05 22:17:26 +1000

    Accidentally removed file.

---
+++ Tangra 3/Model/CoordsAndLocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tangra.Model
+{
+	public class CoordsAndLocation
+	{
+		public double RAHours { get; set; }
+		public double DEDeg { get; set; }
+        public double LatitudeDeg { get; set; }
+        public double LongitudeDeg { get; set; }
+		public double HeightKM { get; set; }
+	}
+}
