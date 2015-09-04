0194c21caf9f4fb162015a3fc8efb1d282a18f07
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2015-09-04 22:00:02 +1000

    Updated version info for release 3.5.2

--- Tangra 3/Properties/AssemblyInfo.cs
+++ Tangra 3/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using Tangra.Helpers;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.5.0.1")]
-[assembly: AssemblyFileVersion("3.5.1")]
-[assembly: ReleaseDate("2015-07-28")]
+[assembly: AssemblyVersion("3.5.0.2")]
+[assembly: AssemblyFileVersion("3.5.2")]
+[assembly: ReleaseDate("2015-09-04")]
 //[assembly: BetaReleaseAttribute()]
 [assembly: TangraCoreVersionRequired("3.0.101")]
 [assembly: TangraVideoVersionRequired("3.0.27")]
