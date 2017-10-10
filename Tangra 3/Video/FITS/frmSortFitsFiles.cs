319f2c8b7d8d50f9d55db8442f8cf2da51e9a005
alexcherney <alex@cher.id.au>
2017-10-10 21:26:44 +1100

    Fixed a TimeStampReader=null bug in the FITS sequence code

--- Tangra 3/Video/FITS/frmSortFitsFiles.cs
+++ Tangra 3/Video/FITS/frmSortFitsFiles.cs
@@ -98,6 +98,8 @@ namespace Tangra.Video.FITS
 
         private void frmSortFitsFiles_Shown(object sender, EventArgs e)
         {
+            TimeStampReader = null;
+
             if (m_FitsFiles.Length > 0)
             {
                 var frm = new frmChooseTimeHeaders(m_FitsFiles[0], GetOrderedFitsFileHash(), m_VideoController);
@@ -120,7 +122,6 @@ namespace Tangra.Video.FITS
                 }
             }
 
-
             m_FitsHeaders = new Header[m_FitsFiles.Length];
             m_FitsTimestamps = new DateTime?[m_FitsFiles.Length];
 
@@ -128,8 +129,7 @@ namespace Tangra.Video.FITS
             pbar.Maximum = m_FitsFiles.Length;
             pbar.Value = 0;
 
-            var fileSizeInfo = new Dictionary<string, FitsFileFormatInfoRecord>();
-            TimeStampReader = null;
+            var fileSizeInfo = new Dictionary<string, FitsFileFormatInfoRecord>();            
                                   
             for (int i = 0; i < m_FitsFiles.Length; i++)
             {
