af66af20d494ff0f80bb3c0c357771eccd9f2997
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2017-10-11 18:23:54 +1100

    Updated version info for hotfix 3.6.10

--- Tangra 3/Properties/AssemblyInfo.cs
+++ Tangra 3/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using Tangra.Helpers;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.6.0.9")]
-[assembly: AssemblyFileVersion("3.6.9")]
-[assembly: ReleaseDate("2017-10-07")]
+[assembly: AssemblyVersion("3.6.0.10")]
+[assembly: AssemblyFileVersion("3.6.10")]
+[assembly: ReleaseDate("2017-10-11")]
 //[assembly: BetaReleaseAttribute()]
 [assembly: TangraCoreVersionRequired("3.0.115")]
 [assembly: TangraVideoVersionRequired("3.0.28")]
