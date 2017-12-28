d9bd92c5e5c470bc2d6f416f9d2e57f37d97620d
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2017-12-28 20:07:14 +1100

    WIP: Adding calibrated photometry tests

---
+++ Tangra.Astrometry.Tests/CalibratedPhotometry/TestPhotometry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Tangra.Astrometry;
+
+namespace Tangra.Tests.CalibratedPhotometry
+{
+    [TestFixture]
+    public class TestPhotometry
+    {
+        [Test]
+        public void Test1()
+        {
+            // TODO:
+            //var fit = new StarMagnitudeFit();
+        }
+    }
+}
