27c5d5542994e55e35d6f91c09417eddf344642c
Hristo Pavlov <hristo_dpavlov@yahoo.com>
2020-05-04 23:21:20 +0100

    Updated release notes and version for release 3.7.1

--- Tangra 3/Properties/AssemblyInfo.cs
+++ Tangra 3/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using Tangra.Helpers;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.7.0.0")]
-[assembly: AssemblyFileVersion("3.7.0")]
-[assembly: ReleaseDate("2020-04-13")]
+[assembly: AssemblyVersion("3.7.0.1")]
+[assembly: AssemblyFileVersion("3.7.1")]
+[assembly: ReleaseDate("2020-05-04")]
 //[assembly: BetaReleaseAttribute()]
 [assembly: TangraCoreVersionRequired("3.0.122")]
 [assembly: TangraVideoVersionRequired("3.0.29")]
